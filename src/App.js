18fa09dbd410dcd8c4e720e9294274d11ac546a6
Cevu01 <vuk.v.stankovic01@gmail.com>
2024-09-29 23:28:21 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [sallery, setSallery] = useState("");
+  const [category, setCategory] = useState("food");
+  const [product, setProduct] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header sallery={sallery} setSallery={setSallery} />
+      <Inputs
+        category={category}
+        setCategory={setCategory}
+        product={product}
+        setProduct={setProduct}
+        quantity={quantity}
+        setQuantity={setQuantity}
+        price={price}
+        setPrice={setPrice}
+      />
+      <Category />
+      <Total />
     </div>
   );
 }
 
-export default App;
+function Header({ sallery, setSallery }) {
+  return (
+    <div className="header">
+      <h1>
+        EXPENSE TRAC<span className="green">K</span>
+        <span className="yellow">E</span>
+        <span className="red">R</span>
+      </h1>
+      <div>
+        <p>How much is your sallery:</p>
+        <input
+          type="number"
+          value={sallery}
+          onChange={(e) => setSallery(Number(e.target.value))}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Inputs({
+  category,
+  setCategory,
+  product,
+  setProduct,
+  quantity,
+  setQuantity,
+  price,
+  setPrice,
+}) {
+  return (
+    <div className="inputs">
+      <label>Category:</label>
+      <select value={category} onChange={setCategory}>
+        <option value="food">Food</option>
+        <option value="transport">Transport</option>
+        <option value="sport">Sport</option>
+        <option value="gifts">Gifts</option>
+        <option value="shopping">Shopping</option>
+      </select>
+
+      <label>Product:</label>
+      <input
+        type="text"
+        value={product}
+        onChange={(e) => setProduct(e.target.value)}
+      />
+
+      <label>Quantity:</label>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      />
+
+      <label>Price:</label>
+      <input
+        type="number"
+        value={price}
+        onChange={(e) => setPrice(Number(e.target.value))}
+      />
+
+      <button className="button">Submit</button>
+    </div>
+  );
+}
+
+function Category() {
+  return (
+    <div>
+      <ul className="categories">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return (
+    <li className="category-container">
+      <h2>Food</h2>
+      <div className="item">
+        <div>
+          <span>2</span>
+          <span>Burger</span>
+          <span>$20</span>
+        </div>
+
+        <div className="item-actions">
+          <button className="edit">Edit</button>
+          <button className="delete">Delete</button>
+        </div>
+      </div>
+      <p>Summary: $60</p>
+    </li>
+  );
+}
+
+function Total() {
+  return (
+    <div className="total">
+      <p>TOTAL: $160</p>
+      <p>Remaining sallery: $840</p>
+    </div>
+  );
+}
